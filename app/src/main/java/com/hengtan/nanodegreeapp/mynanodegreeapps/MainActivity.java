4014facdc24779ba28f4c63b82a2e4a38c751b91
theeheng <heng@H-MacBook-Pro.local>
2015-06-04 00:22:37 +0100

    add button and action

--- app/src/main/java/com/hengtan/nanodegreeapp/mynanodegreeapps/MainActivity.java
+++ app/src/main/java/com/hengtan/nanodegreeapp/mynanodegreeapps/MainActivity.java
@@ -4,14 +4,67 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button spotifyBtn;
+    private Button scoreBtn;
+    private Button libraryBtn;
+    private Button buildBtn;
+    private Button xyzBtn;
+    private Button capstoneBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spotifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.onSpotifyBtnClick((Button) view);
+            }
+        });
+
+        scoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.onScoreBtnClick((Button) view);
+            }
+        });
+
+        libraryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.onLibraryBtnClick((Button) view);
+            }
+        });
+
+        buildBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.onBuildBtnClick((Button) view);
+            }
+        });
+
+        xyzBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.onXyzBtnClick((Button) view);
+            }
+        });
+
+        capstoneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.onCapstoneBtnClick((Button) view);
+            }
+        });
+
     }
 
     @Override
@@ -34,5 +87,47 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onSpotifyBtnClick(View v) {
+        // Display toast message
+        Toast.makeText(getApplicationContext(),
+                "This button will launch my capstone app!",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void onScoreBtnClick(View v) {
+        // Display toast message
+        Toast.makeText(getApplicationContext(),
+                "This button will launch scores app!",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void onLibraryBtnClick(View v) {
+        // Display toast message
+        Toast.makeText(getApplicationContext(),
+                "This button will launch library app!",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void onBuildBtnClick(View v) {
+        // Display toast message
+        Toast.makeText(getApplicationContext(),
+                "This button will launch build it bigger app!",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void onXyzBtnClick(View v) {
+        // Display toast message
+        Toast.makeText(getApplicationContext(),
+                "This button will launch xyz app!",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void onCapstoneBtnClick(View v) {
+        // Display toast message
+        Toast.makeText(getApplicationContext(),
+                "This button will launch my capstone app!",
+                Toast.LENGTH_LONG).show();
     }
 }
